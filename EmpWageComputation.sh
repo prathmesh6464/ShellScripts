dfaa40565dcea59d60b12360b12f8de554463077
prathamesh <prathmesh6464@gmail.com>
2020-02-25 17:31:08 +0530

    MODIFIED : Added part time feature.

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -13,7 +13,13 @@ presentee=$((RANDOM%2))
 #CALCULATING DAILY WAGES
 if(($presentee==1))
 then
-	echo "Full Day Employee Wage Is " $(($WAGE*8))
+	dayPartTimeOrFullTime=$((RANDOM%2))
+	if(($dayPartTimeOrFullTime==1))
+	then
+		echo "Full Day Employee Wage Is " $(($WAGE*8))
+	else
+		echo "Part Time Employee Wage Is " $(($WAGE*4))
+	fi
 else
 	echo "Employee Is Absent Can Not Check Daily Wage "
 fi
