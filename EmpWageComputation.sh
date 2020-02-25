f71cb596495c07ccac13b5ee22acd46f067eeb71
prathamesh <prathmesh6464@gmail.com>
2020-02-25 17:17:47 +0530

    MODIFIED : Added #VARIABLE

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
 printf "Wel Come\n"
+
+
+#VARIABLE
+presentee=$((RANDOM%2))
+
+
+#CHEKING EMPLOYEE IS PRESENT OR ABSENT
+if(($presentee==1))
+then
+	echo "Employee Is Present"
+else
+	echo "Employee Is Absent"
+fi
